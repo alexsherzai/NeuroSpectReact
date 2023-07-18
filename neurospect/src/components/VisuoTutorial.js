4c10b2e333bca03f40c50df690988caad6cb1c6c
Alex Sherzai <sherzaiscience@gmail.com>
2023-07-17 18:56:40 -0700

    VisuoTutorialChanges

--- neurospect/src/components/VisuoTutorial.js
+++ neurospect/src/components/VisuoTutorial.js
@@ -52,12 +52,16 @@ const VisuoTutorial = ({onTimeEnd }) => {
                             <polygon points="125,100 125,150 175,150 175,100" strokeWidth="0" fill="orange" />
                         </svg>
 
-                        <div style={{zIndex:1004, position:'absolute', fontFamily:'Poppins-Regular', stroke:'#FF9417', backgroundColor:'white', borderRadius:'6px', marginTop:'100%'}}>
-							<Text>This is the <strong>original</strong> shape.</Text>
+                        <div className="tut-intro-fade" style={{position: 'absolute', zIndex:1100, marginTop:'78%'}}>
+                            <img src='/TutorialIcons/Arrow1.png'/>
+                        </div>
+
+                        <div className="tut-intro-fade" style={{zIndex:1004, position:'absolute', fontFamily:'Poppins-Regular', border:'solid 2px #FF9417', backgroundColor:'white', borderRadius:'6px', marginTop:'100%'}}>
+							<Text style={{margin: '10px'}}>This is the <strong>original</strong> shape.</Text>
 						</div>
                     </div>
 
-                    <div className="vis-button-container">
+                    <div>
                         <button className="visuoButton">
                             <svg width="150" height="150">
                                 <polygon points="45,70 45,100 75,100 75,70" strokeWidth="0" fill="blue" />
@@ -98,54 +102,67 @@ const VisuoTutorial = ({onTimeEnd }) => {
             }
 
             {iter === 2 && 
+            
             <div onClick={onClickResponse}>
-            <div>
-                <div style={{justifyContent: 'center', alignItems:'center', textAlign:'center', margin:'10px'}}>
-                    <h3 style={{fontFamily:'Poppins-Regular'}}>1/1</h3>
-                    <h3 style={{fontFamily:'Poppins-Regular'}}>Which one of the options is the <span className='highlight'>rotated</span> version of the shape in the middle?</h3>
-                </div>
-                <div style={{justifyContent: 'center', alignItems:'center', textAlign:'center', margin:'10px'}}>
-                    <svg width="200" height="200">
-                        <polygon points="50,50 50,100 100,100 100,50" strokeWidth="0" fill="blue" />
-                        <polygon points="100,50 100,100 150,100 150,50" strokeWidth="0" fill="red" />
-                        <polygon points="100,100 100,150 150,150 150,100" strokeWidth="0" fill="orange" />
-                    </svg>
-                </div>
+                <div>
+                    <div style={{position: 'absolute'}}>
+                    <div style={{justifyContent: 'center', width: '100%', alignItems:'center', textAlign:'center', margin:'10px'}}>
+                        <h3 style={{fontFamily:'Poppins-Regular'}}>1/1</h3>
+                    </div>
 
-                <div className="vis-button-container">
-                    <button className="visuoButton" onClick={buttonWrong}>
-                        <svg width="150" height="150">
-                            <polygon points="45,70 45,100 75,100 75,70" strokeWidth="0" fill="blue" />
-                            <polygon points="75,70 75,100 105,100 105,70" strokeWidth="0" fill="red" />
-                            <polygon points="75,40 75,70 105,70 105,40" strokeWidth="0" fill="orange" />
-                        </svg>
-                    </button>
-                    <button className="visuoButton" onClick={buttonWrong}>
-                        <svg width="150" height="150">
-                            <polygon points="45,70 45,100 75,100 75,70" strokeWidth="0" fill="blue" />
-                            <polygon points="75,70 75,100 105,100 105,70" strokeWidth="0" fill="red" />
-                            <polygon points="75,40 75,70 105,70 105,40" strokeWidth="0" fill="blue" />
-                        </svg>
-                    </button>
-                    <button className="visuoButton" onClick={buttonCorrect}>
-                        <svg width="150" height="150">
-                            <polygon points="75,70 75,100 105,100 105,70" strokeWidth="0" fill="blue" />
-                            <polygon points="45,70 45,100 75,100 75,70" strokeWidth="0" fill="red" />
-                            <polygon points="45,40 45,70 75,70 75,40" strokeWidth="0" fill="orange" />
-                        </svg>
-                    </button>
-                    <button className="visuoButton" onClick={buttonWrong}>
-                        <svg width="150" height="150">
-                            <polygon points="45,70 45,100 75,100 75,70" strokeWidth="0" fill="blue" />
-                            <polygon points="75,70 75,100 105,100 105,70" strokeWidth="0" fill="blue" />
-                            <polygon points="45,40 45,70 75,70 75,40" strokeWidth="0" fill="green" />
-                        </svg>
-                    </button>
-                </div>
+                    <div style={{position: 'absolute', zIndex:1100, marginTop:'80%'}}>
+                        <img src='/TutorialIcons/Arrow2.png'/>
+                    </div>
 
-                
+                    <div style={{zIndex:1004, position:'absolute', fontFamily:'Poppins-Regular', border:'solid 2px #FF9417', backgroundColor:'white', borderRadius:'6px', marginTop:'100%'}}>
+                        <Text style={{margin: '10px'}}>Choose the option that is the <strong>rotated</strong> version of the original shape</Text>
+                    </div>
+
+                    </div>
+
+                    <div>
+
+                        <div style={{height:'45vh', backgroundColor:'black', opacity:'0.8'}}></div>
+
+                        <div className='vis-button-container'>
+                            <button className="visuoButton">
+                                <svg width="150" height="150">
+                                    <polygon points="45,70 45,100 75,100 75,70" strokeWidth="0" fill="blue" />
+                                    <polygon points="75,70 75,100 105,100 105,70" strokeWidth="0" fill="red" />
+                                    <polygon points="75,40 75,70 105,70 105,40" strokeWidth="0" fill="orange" />
+                                </svg>
+                            </button>
+                            <button className="visuoButton">
+                                <svg width="150" height="150">
+                                    <polygon points="45,70 45,100 75,100 75,70" strokeWidth="0" fill="blue" />
+                                    <polygon points="75,70 75,100 105,100 105,70" strokeWidth="0" fill="red" />
+                                    <polygon points="75,40 75,70 105,70 105,40" strokeWidth="0" fill="blue" />
+                                </svg>
+                            </button>
+                            <button className="visuoButton">
+                                <svg width="150" height="150">
+                                    <polygon points="75,70 75,100 105,100 105,70" strokeWidth="0" fill="blue" />
+                                    <polygon points="45,70 45,100 75,100 75,70" strokeWidth="0" fill="red" />
+                                    <polygon points="45,40 45,70 75,70 75,40" strokeWidth="0" fill="orange" />
+                                </svg>
+                            </button>
+                            <button className="visuoButton">
+                                <svg width="150" height="150">
+                                    <polygon points="45,70 45,100 75,100 75,70" strokeWidth="0" fill="blue" />
+                                    <polygon points="75,70 75,100 105,100 105,70" strokeWidth="0" fill="blue" />
+                                    <polygon points="45,40 45,70 75,70 75,40" strokeWidth="0" fill="green" />
+                                </svg>
+                            </button>
+                        </div>
+
+                    </div>
+
+                    <div class="tutorialCoverNoFade"></div>
+
+                    
+                </div>
             </div>
-            </div>
+
             }
 
             {iter === 6 && 
