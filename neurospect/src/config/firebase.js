c4dd244004d2aeb2d9acba4926f51b0686e52c97
Alex Sherzai <sherzaiscience@gmail.com>
2023-07-18 06:15:14 -0700

    Update firebase.js

--- neurospect/src/config/firebase.js
+++ neurospect/src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfigDev = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey: process.env.REACT_APP_API_KEY_DEV,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN_DEV,
+    projectId: process.env.REACT_APP_PROJECT_ID_DEV,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET_DEV,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID_DEV,
+    appId: process.env.REACT_APP_APP_ID_DEV,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID_DEV
 };
 
 const appDev = initializeApp(firebaseConfigDev);
