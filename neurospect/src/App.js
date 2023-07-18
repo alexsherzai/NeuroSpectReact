ec77792c83d509e60dd6b4a05233d478edf9f1ba
Alex Sherzai <sherzaiscience@gmail.com>
2023-07-18 05:44:18 -0700

    keys added

--- neurospect/src/App.js
+++ neurospect/src/App.js
@@ -49,7 +49,6 @@ const App = () => {
     const storeRecall = (score) => {
         setRecSc(score);
     };
-    
 
     const attentionShapes = [
 		<svg width="100" height="100">
--- neurospect/src/config/firebase.js
+++ neurospect/src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfigDev = {
-    apiKey: "AIzaSyBYTlNI-VTKCRNq8xANI0CUkGxxiU8bAbY",
-    authDomain: "neuroverse-dev.firebaseapp.com",
-    projectId: "neuroverse-dev",
-    storageBucket: "neuroverse-dev.appspot.com",
-    messagingSenderId: "473832931878",
-    appId: "1:473832931878:web:ae0afa49e461e90dd29ce7",
-    measurementId: "G-EBL3BGHH6K"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const appDev = initializeApp(firebaseConfigDev);
