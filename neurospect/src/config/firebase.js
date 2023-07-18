baa89df5204155ca1b6f7552df35c61eb150736f
Alex Sherzai <sherzaiscience@gmail.com>
2023-07-18 05:49:04 -0700

    keys added

--- neurospect/src/config/firebase.js
+++ neurospect/src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfigProd = {
-    apiKey: "AIzaSyAz-wddbpROXPhPEeItB01ngri0XUD0XSg",
-    authDomain: "neuroverse-fdf22.firebaseapp.com",
-    projectId: "neuroverse-fdf22",
-    storageBucket: "neuroverse-fdf22.appspot.com",
-    messagingSenderId: "656486393456",
-    appId: "1:656486393456:web:525952f1906aa4967236b5",
-    measurementId: "G-MNS48EZ8GT"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const appProd = initializeApp(firebaseConfigProd);
