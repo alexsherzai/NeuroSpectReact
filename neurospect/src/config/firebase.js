785164ee6d6d9c8c3441e51f8bf42a555e938c68
Alex Sherzai <sherzaiscience@gmail.com>
2023-07-18 16:31:52 -0700

    Visuospatial Score Fix

--- neurospect/src/config/firebase.js
+++ neurospect/src/config/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-console.log(process.env.REACT_APP_AUTH_DOMAIN_DEV);
+console.log("Auth domain: " + process.env.REACT_APP_AUTH_DOMAIN_DEV);
 
 const firebaseConfigDev = {
     apiKey: process.env.REACT_APP_API_KEY_DEV,
