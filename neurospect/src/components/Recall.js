921a0b79eee92bd7db146c302dfca0bb5cf11252
alexsherzai <sherzaiscience@gmail.com>
2023-10-18 01:04:44 -0700

    react-native-voice

--- neurospect/src/components/Recall.js
+++ neurospect/src/components/Recall.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { updateDoc, doc } from 'firebase/firestore';
-import { storage } from '../config/firebase';
-import SpeechRecognition, {useSpeechRecognition} from 'react-speech-recognition';
 import MicrophoneWaveform from './MicrophoneWaveform';
-
+import Voice from 'react-native-voice';
 
 const Recall = ({ recData, storeRec, words, onTimeEnd }) => {
-    const [timeLeft, setTimeLeft] = useState(60);
+    const [timeLeft, setTimeLeft] = useState(6000);
     const [inputWords, setInputWords] = useState('');
     const [correctWords, setCorrectWords] = useState(0);
     const [answeredWords, setAnsweredWords] = useState(['']);
@@ -17,74 +14,46 @@ const Recall = ({ recData, storeRec, words, onTimeEnd }) => {
 
     const [mic, setMic] = useState(false);
 
-    const queryParams = new URLSearchParams(window.location.search)
-    const prolificID = queryParams.get("PROLIFIC_PID");
-    const userID = queryParams.get("userID");
-
-    const {transcript, browserSupportsSpeechRecognition} = useSpeechRecognition();
-    const listeningButton = () => {
-        if(!mic) {
-            SpeechRecognition.startListening({continuous: true, language: 'en-US'});
-            setMic(true);
-        } else {
-            SpeechRecognition.stopListening();
-            setMic(false);
-        }
-        
-    };
-
-    let docName = userID;
-    if(prolificID !== null) {
-        docName = prolificID;
-    } else if(userID === null && prolificID === null) {
-        docName = "noID";
-    }
-
     const AddData = () => {
         let recScore = 0;
 
         for (const [key, value] of Object.entries(wordsDict)) {
-            if(value) {
-                recScore++;
-            }
+        if (value) {
+            recScore++;
+        }
         }
 
-        if(recScore > 10) {
-            recScore = 10;
+        if (recScore > 10) {
+        recScore = 10;
         }
-
 
         storeRec(recScore);
 
-
-        recData(
-            {
-                recall: recScore,
-                recalledWords: wordsDict
-            }
-        );
-    }
+        recData({
+            recall: recScore,
+            recalledWords: wordsDict
+        });
+    };
 
     const enterWord = () => {
-        if(!(inputWords.toLowerCase() in wordsDict)) {
+        if (!(inputWords.toLowerCase() in wordsDict)) {
             setWarning("Wrong Word!");
             setWordAtPoint(inputWords);
-        } else if(wordsDict[inputWords.toLowerCase()] === true && !mic) {
+        } else if (wordsDict[inputWords.toLowerCase()] === true && !mic) {
             setWarning("You already answered this word!");
             setWordAtPoint(inputWords);
-        } else if(wordsDict[inputWords.toLowerCase()] === false) {
+        } else if (wordsDict[inputWords.toLowerCase()] === false) {
             wordsDict[inputWords.toLowerCase()] = true;
             setInputWords('');
-        } 
+        }
     }
 
     useEffect(() => {
-        if(inputWords !== wordAtPoint) {
+        if (inputWords !== wordAtPoint) {
             setWarning('');
         }
 
         console.log("hehe");
-
 
         const timer = setInterval(() => {
             setTimeLeft(oldTime => {
@@ -96,125 +65,87 @@ const Recall = ({ recData, storeRec, words, onTimeEnd }) => {
             });
         }, 1000);
 
-        if(inputWords.charAt(inputWords.length - 1) === ' ') {
+        if (inputWords.charAt(inputWords.length - 1) === ' ') {
             setInputWords(inputWords.slice(0, -1));
         }
 
-        /*
-        const checkWords = () => {
-            let count = 0;
-
-            let included = false;
-
-            for(let i = 0; i < words.length; i ++ ) {
-                
-                if (words[i].toLowerCase() === inputWords.toLowerCase()) {
-
-                    for(let j = 0; j < answeredWords.length; j++) {
-                        if(answeredWords[j].toLowerCase() === inputWords.toLowerCase()) {
-                            included = true;
-                            break;
-                        }
-                    }
-
-                    if(!included) {
-                        count++;
-                        answeredWords.push(words[i]);
-                        setInputWords('');
-                        setWarning('');
-                    }
-                }
-            }
-
-            setCorrectWords(count);
-        };
-
-        if(answeredWords.length === 11) {
-            AddData();
-            onTimeEnd();
-        }
-        */
-
-        if (mic && transcript) {
-            const spokenWords = transcript.trim().split(' ');
-            const newInputWord = spokenWords[spokenWords.length - 1];
-            setInputWords(newInputWord);
-            console.log(newInputWord);
-            enterWord();
+        if (mic) {
+            Voice.start('en-US');
+            Voice.onSpeechResults = (e) => {
+                const newInputWord = e.value[0];
+                setInputWords(newInputWord);
+                console.log(newInputWord);
+                enterWord();
+            };
+        } else {
+            Voice.stop();
         }
 
         let complete = true;
-        for(let word in wordsDict) {
-            if(wordsDict[word] === false) {
+        for (let word in wordsDict) {
+            if (wordsDict[word] === false) {
                 complete = false;
             }
         }
 
-        if(timeLeft <= 1) {
+        if (timeLeft <= 1) {
             complete = true;
         }
 
-        if(complete) {
+        if (complete) {
             AddData();
             onTimeEnd();
         }
 
-        return () => clearInterval(timer);
-    }, [AddData, onTimeEnd, words, mic, transcript]);
-    
+        return () => {
+            clearInterval(timer);
+            Voice.removeAllListeners();
+        };
+    }, [AddData, onTimeEnd, words, mic]);
+
     return (
         <div className='fullGameMargin'>
-            <h1 className='timer'>{timeLeft} sec</h1>
-            <div>
-                <div className="encoding-content">
-                    {Object.entries(wordsDict).map(([word, answered]) => 
-                    
-                    <p className="word">{!answered ? "" : word.charAt(0).toUpperCase() + word.substring(1)}</p>
-                    
-                    )}
+        <h1 className='timer'>{timeLeft} sec</h1>
+        <div>
+            <div className="encoding-content">
+            {Object.entries(wordsDict).map(([word, answered]) => 
+                <p className="word">{!answered ? "" : word.charAt(0).toUpperCase() + word.substring(1)}</p>
+            )}
+            </div>
+        </div>
+
+        <div style={{ height: "10vh" }}></div>
+
+        <div className='recallInterface' style={{ justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
+            {mic === false ? 
+            (<input
+                className='textField'
+                type="text"
+                placeholder="Enter the words"
+                value={inputWords}
+                autoFocus={true}
+                onChange={e => setInputWords(e.target.value)}
+            />) : 
+            (
+                <div>
+                <MicrophoneWaveform/>
                 </div>
+            )
+            }
+            <div style={{ justifyContent: 'center', alignItems: 'center', display: 'flex', marginLeft: '10px'}}>
+            { mic ? 
+                (<button className='pause' onClick={() => setMic(false)}><img src='pause.svg'/></button>) :
+                (inputWords.length === 0 ? 
+                (<button className='mic' onClick={() => setMic(true)}><img src='mic.svg'/></button>) :
+                (<button className='enterButton' onClick={enterWord}><img src='send.svg'/></button>)
+                ) 
+            }
             </div>
+        </div>
 
-            <div style={{height: "10vh"}}>
-
-            </div>
-
-            <div className='recallInterface' style={{ justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
-                { mic === false ? 
-                (<input
-                    className='textField'
-                    type="text"
-                    placeholder="Enter the words"
-                    value={inputWords}
-                    autoFocus={true}
-                    onChange={e => setInputWords(e.target.value)} 
-                />) : 
-                (
-                    <div>
-                        <MicrophoneWaveform/>
-                    </div>
-                )
-                }
-                <div style={{ justifyContent: 'center', alignItems: 'center', display: 'flex', marginLeft: '10px'}}>
-                    
-                    { mic ? 
-                        (<button className='pause' onClick={listeningButton}><img src='pause.svg'/></button>)
-                        :
-                        (inputWords.length === 0 ? 
-                            (<button className='mic' onClick={listeningButton}><img src='mic.svg'/></button>) 
-                            :
-                            (<button className='enterButton' onClick={enterWord}><img src='send.svg'/></button>)
-                        ) 
-                        }
-                    
-                </div>
-            </div>
-            
-
-            <div style={{textAlign:'center', fontFamily:'Poppins-Regular'}}>
-                <h3 style={{color: '#CD3843'}}>{warning}</h3>
-            </div>
-            
+        <div style={{ textAlign: 'center', fontFamily: 'Poppins-Regular' }}>
+            <h3 style={{ color: '#CD3843' }}>{warning}</h3>
+        </div>
         </div>
     );
 };
