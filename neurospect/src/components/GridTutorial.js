53e4217979c5bb41e320cdcb7b385a1789e0a8ab
alexsherzai <sherzaiscience@gmail.com>
2024-04-02 23:58:06 -0700

    Improvements Half

--- neurospect/src/components/GridTutorial.js
+++ neurospect/src/components/GridTutorial.js
@@ -149,8 +149,8 @@ const GridTutorial = ({onTimeEnd }) => {
 
                 <div>
 
-                    <div className="grid-main-shape">
-                        <div className='gridCont' style={{zIndex: "inherit", backgroundColor:"#F4F6FA", borderRadius:"10px", padding: '15px'}}>
+                    <div className="grid-main-shape"  style={{marginTop: "15vh"}}>
+                        <div className='gridCont' style={{zIndex: "inherit", backgroundColor:"#F4F6FA", borderRadius:"10px"}}>
                             <div className="grid" style={{left: 0, right: 0, gridTemplateColumns: 'repeat(3, 1fr)', width: '100%', maxHeight: '350px', minHeight: '300px', gap: '5px'}}>
                                 <div className='cell' onClick={() => cellClicked(0)} key={0}></div>
                                 <div className='cell' onClick={() => cellClicked(1)} key={1}></div>
@@ -165,10 +165,10 @@ const GridTutorial = ({onTimeEnd }) => {
                         </div>
                     </div>
 
-                    <div className="tut-intro-fade" style={{position: "absolute", zIndex:1100, marginLeft: "60%"}}>
+                    <div className="tut-intro-fade" style={{position: "absolute", zIndex:1100, marginLeft: "60%", marginTop: "65vh"}}>
                         <img src="/TutorialIcons/Arrow1.png"/>
                     </div>
-                    <div className="tut-intro-fade" style={{zIndex:1004, position:"absolute", paddingTop:'10px', paddingBottom:'10px', padding: '5px', fontFamily:"Poppins-Regular", border:"solid 2px #FF9417", backgroundColor:"white", borderRadius:"6px", marginTop:"12%", marginLeft: "16%"}}>
+                    <div className="tut-intro-fade" style={{zIndex:1004, position:"absolute", marginTop: "72vh", paddingTop:'10px', paddingBottom:'10px', padding: '5px', fontFamily:"Poppins-Regular", border:"solid 2px #FF9417", backgroundColor:"white", borderRadius:"6px", marginLeft: "16%"}}>
                         <Text style={{padding: '30px', fontSize: "16px"}}>This is a <strong>grid</strong> of squares</Text>
                     </div>
                     
@@ -304,7 +304,7 @@ const GridTutorial = ({onTimeEnd }) => {
 								<div style={{textAlign: 'center', fontFamily: "Poppins-Regular", fontSize: "30px", fontWeight: '600', marginBottom: '10%', color: 'white'}}>
 									Tutorial Over!
 								</div>
-								<button className="buttonSecondary" onClick={() => reset()}>How to Play?</button>
+								<button className="buttonSecondary" onClick={() => reset()}>Play Tutorial Again</button>
 								<button className="buttonNext" onClick={onClickResponse}>Start Playing</button>
 							</div>
 						</div>
